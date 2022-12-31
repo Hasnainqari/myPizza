8bcebc81d0493f4ac1e92d1ac943b56e1c2ca2eb
Hasnain Qari <qarihasnain43@gmail.com>
2022-12-31 20:31:59 +0500

    card 1 ready

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -1,8 +1,7 @@
 import * as React from 'react';
-import { Button, View } from 'react-native';
+import { Button, View, Image } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-// import { Image } from 'react-native';
 import HomeScreen from '../screens/home';
 import ChatScreen from '../screens/chat';
 
@@ -10,13 +9,13 @@ const Drawer = createDrawerNavigator();
 
 export default function App() {
 
-    // var Baket = require("./img/Icon");
+    const Baket = require('../assest/Icon/Vector.png');
 
     return (
         <NavigationContainer>
             <Drawer.Navigator initialRouteName="Home">
-                {/* <Drawer.Screen name="Deliver to Home" component={HomeScreen} options={{ headerRight: () => (<Image source={ Baket } style={{ width: 25, height: 25, marginRight: 20, opacity: 0.7 }}></Image>) }} /> */}
-                <Drawer.Screen name="Deliver to Home" component={HomeScreen} />
+                <Drawer.Screen name="Deliver to Home" component={HomeScreen} options={{ headerRight: () => (<Image source={ Baket } style={{ width: 17, height: 16.15, marginRight: 20 }}></Image>) }} />
+                {/* <Drawer.Screen name="Deliver to Home" component={HomeScreen} /> */}
                 <Drawer.Screen name="Chat" component={ChatScreen} />
             </Drawer.Navigator>
         </NavigationContainer>
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,8 +1,9 @@
-import { Text, View } from 'react-native';
+import { Text, View, Image, TouchableOpacity } from 'react-native';
+// import { TouchableOpacity } from 'react-native-gesture-handler';
 import LinearGradient from 'react-native-linear-gradient';
-// import { Image } from 'react-native';
 const HomeScreen = () => {
-  // var pizzaImg = require('../assest/Card/'); 
+  var pizzaImg = require('../assest/Card/crustone.png');
+
   const B = props => <Text style={{ fontWeight: 'bold' }}>{props.children}</Text>;
   return (
     <View style={{
@@ -18,16 +19,21 @@ const HomeScreen = () => {
         colors={['#F5313F', '#FFAA6C']}>
         <View style={{ padding: 25, marginLeft: 12, height: 170 }}>
           <Text style={{
-            fontSize: 14,
+            fontSize: 17,
+            fontFamily: 'Roboto',
             color: '#ffffff',
-          }}>Hi Jackey !!!
+          }}>Hi Jackey !
           </Text>
           <Text style={{
             color: '#ffffff',
+            fontFamily: 'Roboto',
             fontSize: 25,
+            left: 10,
           }}>What <B>pizza</B> do you
           </Text>
           <Text style={{
+            left: 10,
+            fontFamily: 'Roboto',
             fontSize: 25,
             color: '#ffffff',
           }}>want to try today?
@@ -35,10 +41,99 @@ const HomeScreen = () => {
         </View>
       </LinearGradient>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <View>
-          <Text >
-            {/* <Image source={pizzaImg} style={{width: 180, height: 200}}></Image> */}
-          </Text>
+        <View style={{
+          backgroundColor: 'rgba(255, 255, 255, 0.8)',
+          borderRadius: 20,
+          width: 328,
+          height: 238,
+          marginTop: -325,
+          flexDirection: 'row',
+        }}>
+          <View>
+            <Image source={pizzaImg} style={{
+              width: 200,
+              height: 200,
+              marginTop: 18,
+            }}></Image>
+          </View>
+          <View style={{
+            display: 'flex',
+            marginTop: 45,
+          }}>
+            <View style={{
+              padding: 5,
+              marginLeft: -45,
+            }}>
+              <Text style={{
+                fontSize: 25,
+                fontFamily: 'Roboto',
+                fontWeight: '900',
+                color: '#F5313F',
+                letterSpacing: 0.3,
+              }}>
+                Reorder again ?
+              </Text>
+              <Text style={{
+                fontWeight: '700',
+                fontSize: 14,
+                fontFamily: 'Roboto',
+                textTransform: 'uppercase',
+                color: '#6D6E9C',
+              }}>
+                Small, thin crust,
+
+              </Text>
+              <Text style={{
+                fontWeight: '700',
+                fontSize: 14,
+                lineHeight: 14,
+                fontFamily: 'Roboto',
+                textTransform: 'uppercase',
+                color: '#6D6E9C',
+              }}>
+                tomatoes, basil, cheese
+
+              </Text>
+              <Text style={{
+                fontWeight: 'bold',
+                fontSize: 30,
+                fontFamily: 'Roboto',
+                textTransform: 'uppercase',
+                color: '#6D6E9C',
+              }}>
+                $12
+              </Text>
+              <TouchableOpacity>
+                <LinearGradient
+                  useAngle={true}
+                  angle={135}
+                  angleCenter={{ x: 0.5, y: 0.6 }}
+                  locations={[0.1, 0.6]}
+                  colors={['#F5313F', '#FFAA6C']}
+                  style={{
+                    borderRadius: 20,
+                    marginTop: 3,
+                    padding: 8,
+                    width: 123,
+                    height: 38,
+                    marginRight: 5,
+
+                  }}>
+                  <Text
+                    style={{
+                      borderRadius: 20,
+                      lineHeight: 23,
+                      fontSize: 17,
+                      fontWeight: '900',
+                      color: '#ffffff',
+                      textAlign: 'center'
+                    }}>
+                    Add to Cart
+                  </Text>
+                </LinearGradient>
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
       </View>
     </View>
